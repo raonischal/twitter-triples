85dfeef32f4bfc0a27d6300aa8e1cb15b9e13cb8
Indu Mohanan <imohanan@usc.edu>
2015-04-11 17:55:54 -0700

    Using soundex, returns true of any of the words are similar

--- twitter-triples/string_similarity.py
+++ twitter-triples/string_similarity.py
@@ -3,34 +3,25 @@
 #    reduces it to a list of unique keywords
 #
 import jellyfish
+import sys
 
 class String_Matcher:
     entities=[]
     unique_entities=[]    
 
-    def __init__(self):
-        self.entities=['carter','carters','CARTAH','CARTAR','crater','cater'] 
-        #self.entities=['miliman','milliman']       
-       
-
     def get_unique_entities(self):
-        for entity in self.entities:
-            entity_found=False
-            for word in self.unique_entities:
-                #if jellyfish.metaphone(word) == jellyfish.metaphone(entity):
-                #if (jellyfish.jaro_distance(word)-jellyfish.jaro_distance(entity) >0.98) or (jellyfish.jaro_distance(word)-jellyfish.jaro_distance(entity) < -0.98)
-                if jellyfish.soundex(word) == jellyfish.soundex(entity):
-                    entity_found=True
-                    break
-            if entity_found==False:self.unique_entities.append(entity)
-
-        return self.unique_entities
-
+        args=sys.argv
+        wordList1=args[2].split()
+        wordList2=args[3].split()
+        for entity in wordList1:
+            for word in wordList2:
+                if jellyfish.soundex(entity) == jellyfish.soundex(word):return True
+        return False
+                
 
 if __name__=="__main__":
     matcher=String_Matcher()
-    unique_entities=matcher.get_unique_entities()
-    for entity in unique_entities:
-        print(entity)
+    return matcher.match_strings()
+   
 
     
