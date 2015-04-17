dd84100cf034f44f47ac6ef41220e21f35ecafa5
Indu Mohanan <imohanan@usc.edu>
2015-04-16 22:15:49 -0700

    Gettinnf urls word by word

--- twitter-triples/map_url.py
+++ twitter-triples/map_url.py
@@ -23,33 +23,53 @@ class Wiki_Mapper:
     def map_urls(self):
         Entities={}
         for word in self.proper_entities:
-            if word[1]<5: continue  
+            if word[1]<10: continue  
             formatted_words=[]
             for substring in word[0]:
-                if substring[0]=='#' or substring[0]=='@':
-                    substring=self.FormatWord(substring)
+                substring=self.FormatWord(substring)
+                if substring is None: continue
                 substring=substring.lower()
                 formatted_words.append(substring)
-            keyword=self.select_most_relevant(formatted_words)
-            #wikiUrls=wikipedia.search(keyword)
-            #print("Word is : "+keyword)
-            #print(wikiUrls)
-            #Entities[keyword]=self.SelectUrl(word[0],wikiUrls)
+            print(formatted_words)
+            #keywords=self.select_most_relevant(formatted_words)
+            wikiUrls=[]
+            for word in formatted_words:
+                wikiUrls.extend(wikipedia.search(word))
+            wikiUrls=set(wikiUrls)
+            Entities[formatted_words[0]]=self.SelectUrl(formatted_words[0],wikiUrls)
 
         return Entities
 
     def FormatWord(self,word):
-        word =  re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', word)
-        word = word[1:]
-        word=word.strip()   
-        return word
+        try:
+            word=word.encode("utf-8")
+            word=word.replace("-"," ")
+            word=word.replace("."," ")
+            word=word.replace(","," ")
+            word=word.replace(":"," ")
+            word=word.replace(";"," ")
+            word=word.replace("/"," ")
+            word=word.replace("\\"," ")
+            if word[0]=='#' or word[0]=='@':
+                word =  re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', word)
+                word = word[1:]
+                word=word.strip()   
+            return word
+        except UnicodeEncodeError:
+            return None
 
-    def SelectUrl(self,words,wikiUrls):
+    def SelectUrl(self,searchTerm,wikiUrls):
         words_present={}
+        #print("Search term is "+searchTerm)
         for url in wikiUrls:
+            #print("url is : "+url)
             try:
                 page = wikipedia.page(url)
                 content=page.content
+                title=page.title.lower()
+                similarity=self.comparer.getSimilarity(searchTerm,title.encode("utf-8"))
+                #print("Word, title, similarity: "+searchTerm+" : "+title.encode("utf-8")+" : "+str(similarity))
+                if similarity<0.9: continue
                 countOfEntities=0
                 for collection in self.noun_entities,self.proper_entities:
                     for word in collection:
@@ -60,20 +80,17 @@ class Wiki_Mapper:
                             #print(word[0])                                
                             #print(keyword)
                             countOfEntities+=1
-                title=page.title.lower()
-                best_word=self.select_most_relevant(words).lower()
-                similarity=getSimilarity(best_word,title)
-                #print("Word, title, similarity: "+best_word+" : "+title+" : "+str(similarity))
-                # ignore this page if title not matching
-                if similarity<0.9: continue
+                #best_word=self.select_most_relevant(words)[0][0].lower()
+                #print(str(url))
+                #print(countOfEntities)    
                 words_present[page.url]=countOfEntities
+                #ip=input("Enter")
             except wikipedia.exceptions.DisambiguationError:
                 continue
             except wikipedia.exceptions.PageError:
                 continue
             except wikipedia.exceptions.HTTPTimeoutError:
-                time.sleep(60*20)
-                #print("Url is : "+url)    
+                time.sleep(60*20)    
                
         maxcount=-1
         bestUrl=None
@@ -94,7 +111,7 @@ class Wiki_Mapper:
         return min_word
     
     def select_most_relevant(self,words):
-        print(words)        
+        #print(words)        
         count_similar_words={}
         for word in words:
             score=0
@@ -103,7 +120,10 @@ class Wiki_Mapper:
                if value>0.95: score+=1
             count_similar_words[word]=score
         sorted_words = sorted(count_similar_words.items(), key=operator.itemgetter(1),reverse=True)
-        print(sorted_words)
+        sorted_words= [i[0] for i in sorted_words]
+        size=len(sorted_words)/2
+        if size<2:  return sorted_words
+        return sorted_words[:size]
 
 if __name__=="__main__":
     proper_nouns=['#TheDazedAndConfusedTour','chicago','#UKWantsJakeMiller']
